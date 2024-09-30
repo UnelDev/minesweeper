c532c53ca74ae7fd0ad9bfc67e8d243a9ae69acd
Wiwok <emil.dechenaud@gmail.com>
2024-09-30 17:04:32 +0200

    Upgraded time function

--- src/Utils/clearTime.ts
+++ src/Utils/clearTime.ts
@@ -1,13 +1,17 @@
 function clearTime(time: number) {
 	const seconds = time % 60;
-	const minutes = Math.floor(time / 60);
+	const minutes = Math.floor((time / 60) % 60);
+	const hours = Math.floor(time / 3600);
 
-	if (minutes) {
-		if (seconds) {
-			return minutes + (minutes === 1 ? 'min ' : 'mins ') + seconds;
-		} else {
-			return minutes + (minutes === 1 ? 'min' : 'mins');
-		}
+	if (hours) {
+		return (
+			hours +
+			'h ' +
+			(minutes ? minutes + (minutes === 1 ? 'min ' : 'mins ') : '') +
+			(seconds ? seconds + 's' : '')
+		);
+	} else if (minutes) {
+		return minutes + (minutes === 1 ? 'min ' : 'mins ') + (seconds ? seconds + 's' : '');
 	} else {
 		return seconds + 's';
 	}
